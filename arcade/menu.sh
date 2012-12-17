da4dc5077c21d6e46d67c3dcb5224b5ce9699f6d
Andrew Seward <awseward1@gmail.com>
2012-12-17 01:27:59 -0800

    menu.sh now checks which emulator to use instead of just trying both like an idiot

--- arcade/menu.sh
+++ arcade/menu.sh
@@ -9,12 +9,39 @@
 #
 ######
 
-# create array of roms
-GAMES=$(ls /home/pi/.advance/rom/ | sed -e 's/\.[a-z]*//' | sed -e 's/bios//')
-GAMESARRAY=($GAMES)
+if [ $1 -a $1 == "update" ]; then
+  echo UPDATING GAMES...
+  sleep 0.2
 
-NEOGEO=$(ls /home/pi/.gngeo/rom/ | sed -e 's/\.[a-z]*//')
-NEOGEOARRAY=($NEOGEO)
+  # create arrays of advmame, gngeo roms
+  ADVGAMES=$(ls /home/pi/.advance/rom/ | sed -e 's/\.[a-z]*//' | sed -e 's/bios//')
+  ADVGAMESARRAY=($ADVGAMES)
+
+  NEOGAMES=$(ls /home/pi/.gngeo/rom/ | sed -e 's/\.[a-z]*//' | sed -e 's/neogeo//')
+  NEOGAMESARRAY=($NEOGAMES)
+
+  # clear out /home/pi/arcade/common_roms/
+  rm -f /home/pi/arcade/common_roms/*
+
+  # repopulate it
+  for game in "${ADVGAMESARRAY[@]}"
+  do
+    echo adding $game
+    touch /home/pi/arcade/common_roms/$game
+    sleep 0.01
+  done
+  
+  for game in "${NEOGAMESARRAY[@]}"
+  do
+    echo adding $game
+    touch /home/pi/arcade/common_roms/$game
+    sleep 0.01
+  done
+fi
+
+# create array of all games
+ALLGAMES=$(ls /home/pi/arcade/common_roms/)
+ALLGAMESARRAY=($ALLGAMES)
 
 # create array of descriptions
 OLD_IFS=$IFS
@@ -24,7 +51,7 @@ IFS=$OLD_IFS
 
 # create aggregate array to feed to whiptail
 CHOICES=()
-for((i=0; i<${#GAMESARRAY[@]}; i++))
+for((i=0; i<${#ALLGAMESARRAY[@]}; i++))
 do
   CHOICES+=("$i" "${HUMANTITLES[$i]}")
 done
@@ -37,14 +64,19 @@ CHOICES+=("999" "System shut down") # Add a shutdown option
 ######
 
 function launch {
-  echo LAUNCHING ADVMAME
-  advmame ${GAMESARRAY[$1]}
+  if [ $( ls /home/pi/.advance/rom | grep -m 1 "${ALLGAMESARRAY[$1]}") ]; then
+    advmame ${ALLGAMESARRAY[$1]}
+  elif [ $( ls /home/pi/.gngeo/rom | grep -m 1 "${ALLGAMESARRAY[$1]}") ]; then
+    gngeo ${ALLGAMESARRAY[$1]}
+  fi
 }
 
-function launch_neo {
-  echo LAUNCHING GNGEO
-  gngeo ${GAMESARRAY[$1]}
-  #gngeo ${NEOGEOARRAY[$1]}
+function error {
+  whiptail --msgbox "There was an error launching ${HUMANTITLES[$1]}.\nLooks like you broke it..." 20 60 1
+}
+
+function exit_warning {
+  whiptail --infobox "I'm afraid I can't let you do that, Dave...\n\nQuitting to the CLI without a keyboard may not be the best idea." --title "HAL9000" 20 60 1  
 }
 
 function finish {
@@ -64,6 +96,7 @@ function turn_off {
 # run loop
 #
 ######
+
 while true; do
   CHOICE=$(whiptail --menu "\n Select a Game" 30 80 20 --cancel-button Info --ok-button Select \
     "${CHOICES[@]}" \
@@ -75,14 +108,13 @@ while true; do
     if [ $CHOICE -eq 999 ]; then
       turn_off
     else
-      # this needs to be re-worked...
-      launch_neo $CHOICE || launch $CHOICE || whiptail --msgbox "There was an error launching ${HUMANTITLES[$CHOICE]}.\nLooks like you broke it..." 20 60 1
+      launch $CHOICE || error $CHOICE
     fi
   elif [ $TERM != "linux" ]; then
     clear
     exit 1
   else
-    whiptail --infobox "I'm afraid I can't let you do that, Dave...\n\nQuitting to the CLI without a keyboard may not be the best idea." --title "HAL9000" 20 60 1
+    exit_warning
     sleep 3
   fi
 done
